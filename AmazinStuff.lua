b6839320e4031945e3936d4c77ae0df69305d52e
AmazinDooD <153538539+AmazinDooD@users.noreply.github.com>
2025-01-01 15:51:29 +0000

    Update AmazinStuff.lua

--- AmazinStuff.lua
+++ AmazinStuff.lua
@@ -4,7 +4,7 @@
 --- MOD_AUTHOR: [AmazinDooD]
 --- MOD_DESCRIPTION: A simple-ish mod with a bunch of my ideas. Requires JenLib.
 --- BADGE_COLOR: 33CC94
---- DEPENDENCIES: [JenLib, rarlib]
+--- DEPENDENCIES: [JenLib]
 --- PREFIX: amazin
 
 -- deprecated header, i know, i know 
